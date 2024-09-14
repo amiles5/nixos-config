0e16ca2c4f28337784b5161478f917554dcc85b3
amiles5 <anthony.miles@gmail.com>
2024-09-14 15:09:28 +0100

    firewall add for cups

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -6,8 +6,8 @@
     nameservers = [ "8.8.8.8" "8.8.4.4" "1.1.1.1" ];
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 22 80 443 59010 59011 ];
-      allowedUDPPorts = [ 59010 59011 ];
+      allowedTCPPorts = [ 631 22 80 443 59010 59011 ];
+      allowedUDPPorts = [ 631 59010 59011 ];
     };
   };
 
