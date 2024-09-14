7bfe5bb685efd84aab6b80c3951a176b5302f928
Frost-Phoenix <67cyril6767@gmail.com>
2024-09-14 09:10:08 +0100

    test

--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -8,32 +8,20 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" "sdhci_pci" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "ahci" "thunderbolt" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/7e30a88b-7f10-4cc8-903c-cad2891d6263";
+    { device = "/dev/disk/by-uuid/4bf4b1fc-e139-4023-b101-74c49912948e";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/3534-09DD";
+    { device = "/dev/disk/by-uuid/412D-6BBC";
       fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
-
-  fileSystems."/mnt/roon_data" =
-    { device = "//192.168.1.250/roon_data_yakov";
-      fsType = "cifs";
-      options = [ "username=milesj" "password=fasterdoodlefridge" "uid=1000" "gid=100"];
-    };
-
-   fileSystems."/mnt/backup_images" =
-    { device = "//192.168.1.250/backup_images";
-      fsType = "cifs";
-      options = [ "username=milesj" "password=fasterdoodlefridge" "uid=1000" "gid=100"];
+      options = [ "fmask=0022" "dmask=0022" ];
     };
 
   swapDevices = [ ];
@@ -43,9 +31,9 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
