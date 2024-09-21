91bb7e168e3a0d290c7b4870dddaf8c7df8f7d99
amiles5 <anthony.miles@gmail.com>
2024-09-21 10:57:42 +0100

    add backgrould imahe to swaylock

--- modules/home/swaylock.nix
+++ modules/home/swaylock.nix
@@ -4,25 +4,20 @@
     enable = true;
     package = pkgs.swaylock-effects;
     settings = {
-      clock = true;
+      image = "~/Pictures/wallpapers/others/Fantasy-Hongkong.png";
+      clock=true;
       daemonize=true;
       # timestr="%H:%M";
       datestr = "";
-      screenshots = true;
+#     screenshots = true;
       ignore-empty-password=true;
-      
       indicator = true;
-#     indicator-radius = 111;
       indicator-radius = 200;
-#     indicator-thickness = 9;
       indicator-thickness = 20;
-      
       effect-blur = "7x5";
       effect-vignette = "0.75:0.75";
       effect-pixelate = 5;
-      
       font="JetBrainsMono Nerd Font";
-
       text-wrong-color="FBF1C7FF";
       text-ver-color="FBF1C7FF";
       text-clear-color="FBF1C7FF";
