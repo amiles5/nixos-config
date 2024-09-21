9e370300c87a6e42baee90494e60d108ae8c400a
amiles5 <anthony.miles@gmail.com>
2024-09-21 16:57:16 +0100

    chage idle time

--- modules/home/hyprland/hypridle.nix
+++ modules/home/hyprland/hypridle.nix
@@ -19,7 +19,7 @@ general {
 
 # Screenlock
  listener {
-     timeout = 600                    # 10 min
+     timeout = 1800                    # 30 min
      on-timeout = loginctl lock-session # command to run when timeout has passed
      # on-resume = notify-send "Welcome back to your desktop!"  # command to run when activity is detected after timeout has fired.
      on-resume = wpaperd -d 
@@ -27,7 +27,7 @@ general {
 # Turn off screen 
 # (disabled by default)
    listener {
-       timeout = 630                            # 10.5 min
+       timeout = 1830                            # 30.5 min
        on-timeout = hyprctl dispatch dpms off  # command to run when timeout has passed
        on-resume = hyprctl dispatch dpms on    # command to run when activity is detected after timeout has fired.
  }
