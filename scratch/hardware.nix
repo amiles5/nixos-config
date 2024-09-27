b9b6d09c6bc8dc78b02f84e90adf026b965cceea
amiles5 <anthony.miles@gmail.com>
2024-09-27 10:11:27 +0100

    fix mount points

--- scratch/hardware.nix
+++ scratch/hardware.nix
@@ -27,25 +27,21 @@
   fileSystems."/mnt/backup_images" =
     { device = "//192.168.1.250/backup_images";
       fsType = "cifs";
-      options = [ "credentials=/etc/samba/cred" "uid=1000" "gid=100"];
     };
 
   fileSystems."/mnt/roon_data" =
     { device = "//192.168.1.250/roon_data_yakov";
       fsType = "cifs";
-      options = [ "credentials=/etc/samba/cred" "uid=1000" "gid=100"];
     };
 
   fileSystems."/mnt/ventoy-iso" =
     { device = "/dev/disk/by-uuid/1C11-749A";
       fsType = "exfat";
-      options = [ "noauto"];
     };
 
-  fileSystems."/mnt/ventoy-img" =
+  fileSystems."/mnt/bbbb" =
     { device = "/dev/disk/by-uuid/230bf232-6d96-4e7d-bae2-9144f758d41a";
       fsType = "ext4";
-      options = [ "noauto"];
     };
 
   swapDevices = [ ];
